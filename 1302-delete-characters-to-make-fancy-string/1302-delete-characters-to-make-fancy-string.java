66fca0707fb0aa2d28aaedb1355c1afebfa5d0e9
Bhargav Krishna <chiruvolubhargavkrishna@gmail.com>
2025-07-21 19:03:01 +0530

    Time: 14 ms (100%), Space: 45.7 MB (60.35%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,19 +1,22 @@
+//\U0001d4df\U0001d4f5\U0001d4ee\U0001d4ea\U0001d4fc\U0001d4ee \U0001d4e4\U0001d4df\U0001d4e5\U0001d4de\U0001d4e3\U0001d4d4 \U0001d4f2\U0001d4ef \U0001d4f2\U0001d4fd \U0001d4f1\U0001d4ee\U0001d4f5\U0001d4f9\U0001d4fc\U0001f44d\U0001f3fb
 class Solution {
     public String makeFancyString(String s) {
-        StringBuilder ans = new StringBuilder();
-        ans.append(s.charAt(0));
-        int n = s.length(), cnt = 1;
-        for (int i = 1; i < n; i++) {
-            if (s.charAt(i) == ans.charAt(ans.length() - 1)) {
-                cnt++;
-                if (cnt < 3) {
-                    ans.append(s.charAt(i));
-                }
-            } else {
-                cnt = 1;
-                ans.append(s.charAt(i));
+        char[] chars = s.toCharArray();
+        char last = chars[0];
+        int count = 1;
+        int pos = 1;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] != last) {
+                last = chars[i];
+                count = 0;
             }
+
+            if (++count > 2) continue;
+
+            chars[pos++] = chars[i];
         }
-        return ans.toString();
+
+        return new String(chars, 0, pos);
     }
 }
